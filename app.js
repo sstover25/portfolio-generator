cb66ca460311e214a4156fc28d76f26123850f8a
SSto <95592688+sstover25@users.noreply.github.com>
2022-02-14 17:19:49 -0600

    added HTML generator and modularized code

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require("fs");
+const generatePage = require("./src/page-template.js");
 
-const printProfileData = (profileDataArr) => {
-  for (let i = 0; i < profileDataArr.length; i++) {
-    console.log(profileDataArr[i]);
-  }
+const profileDataArgs = process.argv.slice(2);
 
-  console.log("================");
+const [name, github] = profileDataArgs;
 
-  profileDataArr.forEach((profileItem) => console.log(profileItem));
-};
-printProfileData(profileDataArgs);
+fs.writeFile("./index.html", generatePage(name, github), (err) => {
+  if (err) throw new Error(err);
+
+  console.log("Portfolio complete! Check out index.html to see the output!");
+});
+
+// code from module 9.1
+// const printProfileData = (profileDataArr) => {
+//   for (let i = 0; i < profileDataArr.length; i++) {
+//     console.log(profileDataArr[i]);
+//   }
+
+//   console.log("================");
+
+//   profileDataArr.forEach((profileItem) => console.log(profileItem));
+// };
+// printProfileData(profileDataArgs);
