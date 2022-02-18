0909f0a1adb9a9c11d7757fa8208608a58ebce4d
SSto <95592688+sstover25@users.noreply.github.com>
2022-02-18 13:25:21 -0600

    added gitignore and installed inquirer

--- app.js
+++ app.js
@@ -1,24 +1,22 @@
-const fs = require("fs");
-const generatePage = require("./src/page-template.js");
+const inquirer = require("inquirer");
 
-const profileDataArgs = process.argv.slice(2);
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your name?",
+    },
+  ])
+  .then((answers) => console.log(answers));
 
-const [name, github] = profileDataArgs;
+// const fs = require("fs");
+// const generatePage = require("./src/page-template.js");
 
-fs.writeFile("./index.html", generatePage(name, github), (err) => {
-  if (err) throw new Error(err);
+// const pageHTML = generatePage(name, github);
 
-  console.log("Portfolio complete! Check out index.html to see the output!");
-});
+// fs.writeFile("./index.html", pageHTML, (err) => {
+//   if (err) throw err;
 
-// code from module 9.1
-// const printProfileData = (profileDataArr) => {
-//   for (let i = 0; i < profileDataArr.length; i++) {
-//     console.log(profileDataArr[i]);
-//   }
-
-//   console.log("================");
-
-//   profileDataArr.forEach((profileItem) => console.log(profileItem));
-// };
-// printProfileData(profileDataArgs);
+//   console.log("Portfolio complete! Check out index.html to see the output!");
+// });
